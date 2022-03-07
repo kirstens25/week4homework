f32f06d4aad805773fadbd941087b89c80e4801d
Kirsten Segavac <kirstensegavac@gmail.com>
2022-03-07 21:04:31 +0800

    question title showing when start button pressed, having issues getting the list items to show up (answers)

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,89 @@
 const buttonStart = document.getElementById('start-button');
 const sectionQuiz = document.getElementById('quiz-section');
 const sectionLanding = document.getElementById('landing');
+const sectionTimer = document.getElementById('timer-section');
+const spanTime = document.getElementById('span-time');
+const sectionInitials = document.getElementById('initials');
+const sectionSummary = document.getElementById('summary-section')
+const spanScore = document.getElementById('span-score')
+const questionTitle = document.getElementById('question-title')
+const questionChoice = document.getElementById('question-choices')
+const sectionHighScore = document.getElementById('section-highscore')
+const buttonYourScore = document.getElementById('your-score')
 
+let = timerId = null;
+let = timeRemaining = 60;
+
+let = currentQuestion = 0;
+
+spanTime.textContent = timeRemaining;
 
 // When user clicks on button quiz starts - first question appears
-buttonStart.addEventListener('click', function(event) {
+buttonStart.addEventListener('click', function (event) {
     // first question appears
     sectionQuiz.classList.remove('hide');
     // hide landing page
-    sectionLanding.classList.add('hide')})
+    sectionLanding.classList.add('hide');
 
     // timer starts
+    startTimer();
+
+    showQuestion(currentQuestion);
+})
+// TIMER
+
+function startTimer() {
+    // Show section-timer
+    sectionTimer.classList.remove('hide');
+    // countdown span-time as seconds pass
+    timerId = setInterval(function () {
+        timeRemaining = timeRemaining - 1
+        spanTime.textContent = timeRemaining
+
+        // if time remaining < 0
+        if (timeRemaining <= 0) {
+            // end game
+            endGame()
+        }
+        // end game
+    }, 1000);
+
+}
+
+function showQuestion(questionIndex) {
+    const question = questions[questionIndex];
+
+    const title = question.title;
+    const choices = question.choices;
 
 
+    // Show question title
+    questionTitle.textContent = title;
+    // clear question choices 
+    questionChoice.textContent = "";
+    // loop through choices
+    // generate li for each choice
+    for (let index = 0; index < choices.length; index++) {
+        const choice = choices[index];
 
+        const li = document.createElement('li');
+        const button = document.createElement('button');
+
+        button.classList.add('answer-button')
+        button.textContent = choice.title;
+        button.addEventListener('click', checkAnswer);
+        li.appendChild(button);
+
+        questionChoice.appendChild(li)
+    }
+};
+// showQuestion(0)
 // SELECTION OPTIONS
 // when user clicks correct answer show success and add point
-
+// title - string
+// choices
+// title
+// correct answer y/n?
 // when user clicks incorrect answer show wrong and deduct 5 seconds
 
 // if user does not click any answer, timer will run out and game over screen appears
@@ -29,15 +96,25 @@ buttonStart.addEventListener('click', function(event) {
 
 
 // END GAME (summary page)
-// show end game screen 
-// timer stops
-// 1. show their score (number of questions correctly answered)
-// 2. prompt user to enter their intials 
-// 3. user press submit
+function endGame() {
 
-// save initials and score to local storage
-// go to high score page
+    // timer stops
+    clearInterval(timerId);
 
+    // Hide question
+    sectionQuiz.classList.add('hide');
+    // show their score (number of questions correctly answered)
+
+    // prompt user to enter their intials
+    sectionSummary.classList.remove('hide');
+    // 3. user press submit
+
+    // save initials and score to local storage
+    // go to high score page
+    // buttonYourScore.addEventListener('click', function (event) {
+    // sectionHighScore.classList.remove('hide');
+    // })
+}
 // HIGHSCORE Page
 // Show list of high scores
 
@@ -46,4 +123,4 @@ buttonStart.addEventListener('click', function(event) {
 
 // User clicks on Clear button
 // Clear local storage
-// Clear DOM)))
+// Clear DOM 
---
+++ scripts/questions.js
@@ -0,0 +1,28 @@
+const questions = [
+    {
+        title: "How are you?",
+        choices: [
+            {
+                title: "good",
+                isAnswer: true,
+            },
+            {
+                title: "bad",
+                isAnswer: false,
+            },
+        ],
+    },
+    {
+        title: "How are you?",
+        choices: [
+            {
+                title: "good",
+                isAnswer: true,
+            },
+            {
+                title: "bad",
+                isAnswer: false,
+            },
+        ],
+    },
+];
